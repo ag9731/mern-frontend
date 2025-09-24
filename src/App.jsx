8a02c23a7a7655acbe0b60c3213766a8cc24e456
ag9731 <goravanakollakash@gmail.com>
2025-09-24 18:09:27 +0530

    Initiaal Commit - React frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Login from './api/components/Login'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import RequireAuth from './RequireAuth'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Login from './api/components/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RequireAuth from './RequireAuth';
 import Dashboard from './api/components/Dashboard';
-import Register from './api/components/Register'
-import ForgotPassword from './api/components/ForgotPassword'
-import ResetPassword from './api/components/ResetPassword'
+import Register from './api/components/Register';
+import ResetPassword from './api/components/ResetPassword';
 import "./index.css";
+import ForgotPassword from './api/components/ForgotPassword';
 
 function App() {
   const [count, setCount] = useState(0)
