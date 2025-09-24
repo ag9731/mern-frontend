13c1f93fb0349e3c2551ba570c21c843dc16cbaf
ag9731 <goravanakollakash@gmail.com>
2025-09-24 19:36:35 +0530

    updated aapi base url

--- src/api/components/Dashboard2.jsx
+++ src/api/components/Dashboard2.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
 const Dashboard2 = () => {
-  return (
-   <div>
-        <h1>dashboard</h1>
-    </div>
-  )
-}
+  const { isAuthenticated, loading } = useSelector((state) => state.auth);
 
-export default Dashboard2
+  if (loading) return <div>Loading...</div>;
+
+  if (!isAuthenticated) return <Navigate to="/login" replace />;
+
+  return <div>Dashboard Content</div>;
+};
+
+export default Dashboard2;
